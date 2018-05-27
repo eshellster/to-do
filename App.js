dd65593e08bfc83869c82ac5a8ea0ef3fd2dd9a1
eshell <eshellster@gmail.com>
2018-05-27 20:20:02 +0900

    todo 객체생성 uuid 인스톨

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
 import ToDo from './ToDo';
 import { AppLoading } from 'expo';
+import uuidv1 from 'uuid/v1';
+
 const { height, width } = Dimensions.get('window');
 
 export default class App extends React.Component {
 	state = {
 		newToDo: '',
 		loadedToDos: false
+	};
+	componentDidMount = () => {
+		this._loadToDos();
 	};
 	render() {
 		const { newToDo, loadedToDos } = this.state;
@@ -27,6 +32,7 @@ export default class App extends React.Component {
 						placeholderTextColor={'#999'}
 						returnKeyType={'done'}
 						autoCorrect={false}
+						onSubmitEditing={this._addToDo}
 					/>
 					<ScrollView contentContainerStyle={styles.toDos}>
 						<ToDo text={"Hello I'm a To Do!"} />
@@ -39,6 +45,29 @@ export default class App extends React.Component {
 		this.setState({
 			newToDo: text
 		});
+	};
+	_loadToDos = () => {
+		this.setState({
+			loadedToDos: true
+		});
+	};
+
+	// 입력란을 깨끗이 지워 다음 입력을 준비
+	_addToDo = () => {
+		const { newToDo } = this.state;
+		if (newToDo !== '') {
+			this.setState((prevState) => {
+				const ID = uuidv1();
+				const newToDoObject = {
+					[ID]: {
+						id: ID,
+						isCompleted: false,
+						text: newToDo,
+						createdAt: Date.now()
+					}
+				};
+			});
+		}
 	};
 }
 
